fd0a685832b27b96242fb09c404e8e0b3fd6bda1
Yash <yashkumarpal1988@gmail.com>
2025-11-20 14:57:14 +0530

    uppercase pipe used

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pipes-angular');
+  protected readonly title = signal('Code step by step');
 }
