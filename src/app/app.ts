c0c51fbb0b6407e17f18e1050168d6d41a1b1576
Yash <yashkumarpal1988@gmail.com>
2025-11-20 14:55:21 +0530

    First Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
